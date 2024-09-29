3ad632f4bf9869d33472b89f9666e9b77e23b064
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-09-29 18:13:37 +0400

    Additional Files created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,110 @@
 import "./App.css";
+import { useState, useRef, useEffect } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import MainSection from "./components/mainSection.js";
+import HomePage from "./components/HomePage.js";
+import Header from "./components/Header.js";
+import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
+import { MessageTypes } from "./utils/presets";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [downloading, setDownloading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [finished, setFinished] = useState(false);
+
+  const isAudioAvailable = file || audioStream;
+
+  function handleAudioReset() {
+    setFile(null);
+    setAudioStream(null);
+  }
+
+  const worker = useRef(null);
+
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("./utils/whisper.worker.js", import.meta.url),
+        {
+          type: "module",
+        }
+      );
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case "DOWNLOADING":
+          setDownloading(true);
+          console.log("DOWNLOADING");
+          break;
+        case "LOADING":
+          setLoading(true);
+          console.log("LOADING");
+          break;
+        case "RESULT":
+          setOutput(e.data.results);
+          console.log(e.data.results);
+          break;
+        case "INFERENCE_DONE":
+          setFinished(true);
+          console.log("DONE");
+          break;
+      }
+    };
+
+    worker.current.addEventListener("message", onMessageReceived);
+
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
+  });
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000;
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+    const response = await file.arrayBuffer();
+    const decoded = await audioCTX.decodeAudioData(response);
+    const audio = decoded.getChannelData(0);
+    return audio;
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) {
+      return;
+    }
+
+    let audio = await readAudioFrom(file ? file : audioStream);
+    const model_name = `openai/whisper-tiny.en`;
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name,
+    });
+  }
   return (
     <div className="bg-gradient-to-r from-blue-50 to-transparent text-slate-700 text-sm sm:text-base">
-      <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
+      <div className="flex flex-col max-w-[1000px] mx-auto w-full">
         <section className="min-h-screen flex flex-col">
-          <header className="flex items-center justify-between gap-4">
-            <h1>
-              Free <span className="text-blue-400">Scribe</span>
-            </h1>
-            <button className="flex items-center gap-2">
-              <p>New</p>
-              <i class="fa-solid fa-plus"></i>
-            </button>
-          </header>
-          <main className="flex-1"></main>
+          <Header />
+          {output ? (
+            <Information output={output} finished={finished} />
+          ) : loading ? (
+            <Transcribing />
+          ) : isAudioAvailable ? (
+            <FileDisplay
+              handleFormSubmission={handleFormSubmission}
+              handleAudioReset={handleAudioReset}
+              file={file}
+              audioStream={audioStream}
+            />
+          ) : (
+            <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+          )}
         </section>
         <MainSection />
         <footer></footer>
---
+++ src/components/Transcribing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Transcribing = () => {
+  return <div>Transcribing</div>;
+};
